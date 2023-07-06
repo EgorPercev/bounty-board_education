7ff2d47088abbf9dba35f0acdefa1d180c156ae1
egor.pr@hectasoft.ru <egor.pr@hectasoft.ru>
2023-07-06 20:43:22 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+const val heroName = "MAdrigal"
+
+fun main(){
+    println("Hello, world!")
+    var playerLevel = 4
+    println(playerLevel)
+
+    
+    println(heroName)
+    println("The hero embarks on her2 journey to locate the enchanted sword.")
+    playerLevel = 5
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel++
+    println(playerLevel)
+}
